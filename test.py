ef73970e646abaa61f412377fc9056a1397c3041
akashpotturi <akashpotturi@gmail.com>
2025-03-11 12:21:00 +0530

    test run

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-with open('Trace\First500.txt','r') as f:
-    for line in f:
-        data=line.split(',')
-        print(data[2])
+from rectpack import newPacker
+
+rectangles = [(100, 30), (40, 60), (30, 30),(70, 70), (100, 50), (30, 30)]
+bins = [(300, 450), (80, 40), (200, 150)]
+
+packer = newPacker()
+
+# Add the rectangles to packing queue
+for r in rectangles:
+	packer.add_rect(*r)
+
+# Add the bins where the rectangles will be placed
+for b in bins:
+	packer.add_bin(*b)
+
+# Start packing
+packer.pack()
+print(len(packer))
